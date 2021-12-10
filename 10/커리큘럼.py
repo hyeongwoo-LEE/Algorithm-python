3d31b3031f2cc8a0a156d0f0741bc7ba9c23fff0
hyeongwoo-LEE <lhw970804@naver.com>
2021-12-11 01:05:41 +0900

    커리큘럼

--- 10/커리큘럼.py
+++ 10/커리큘럼.py
@@ -1,47 +1,52 @@
 from collections import deque
+import copy
 
+#강의 수
 n = int(input())
 
+#간선 정보 테이블
 graph = [[] for _ in range(n + 1)]
 
+#차수 테이블
 indegree = [0 for i in range(n + 1)]
+
+#강의 시간 테이블
 cost = [0 for i in range(n + 1)]
 
+#강의 시간, 선수과목 정보 입력
 for i in range(1, n + 1):
-    array = list(map(int, input().split()))
+    data = list(map(int, input().split()))
     j = 0
-    while array[j] != -1:
+    while data[j] != -1:
         if j == 0:
-            cost[i] = array[0]
+            cost[i] = data[0]
         else:
-            graph[array[j]].append((cost[array[j]], i))
+            graph[data[j]].append(i)
             indegree[i] += 1
         j += 1
-print(indegree)
+
 
 
 def topology_sort():
     q = deque()
-    answer = [0 for _ in range(n + 1)]
-    print("graph:", graph)
+    answer = copy.deepcopy(cost)
+
+    #차수 0인 강의 큐에 추가
     for i in range(1, n + 1):
         if indegree[i] == 0:
             q.append(i)
-
-    print(q)
 
     while q:
         now = q.popleft()
 
         for i in graph[now]:
-            indegree[i[1]] -= 1
-            answer[i[1]] = max(answer[i[1]], i[0])
+            indegree[i] -= 1
+            answer[i] = max(answer[i], answer[now]+cost[i])
 
-            if indegree[i[1]] == 0:
-                q.append(i[1])
+            if indegree[i] == 0:
+                q.append(i)
 
-    print(answer)
-    print(cost)
-
+    for i in range(1,len(answer)):
+        print(answer[i])
 
 topology_sort()
