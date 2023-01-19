bca1f8603bb5d8c426da049dbb1484cc9fca7aa9
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2023-01-19 18:42:23 +0900

    v2_1032_명령 프롬프트

---
+++ baekjoon_v2/1032_명령 프롬프트.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+data = []
+
+for _ in range(n):
+    data.append(input())
+
+answer = list(data[0])
+
+for i in range(1, len(data)):
+    for j in range(len(answer)):
+        if answer[j] != data[i][j]:
+            answer[j] = '?'
+
+print("".join(answer))
