a6fb51068bd2230f371d155c69c75f1b2155bf21
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2023-02-01 01:53:14 +0900

    v2_1463_1로만들기

---
+++ baekjoon_v2/1463_1로만들기.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+d = [0] * (n+1)
+
+d[1] = 0
+
+for i in range(2,n+1):
+
+  #1로 빼기
+  d[i] = d[i-1] + 1
+
+  #2으로 나누기
+  if i%2 == 0:
+    d[i] = min(d[i], d[i//2]+1)
+
+  #3으로 나누기
+  if i%3 == 0:
+    d[i] = min(d[i], d[i//3]+1)
+
+
+print(d[n])
